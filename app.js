394d56a5798a2ce5411846dedc5feefd926609fd
Saurav R <121216190+RSauravR@users.noreply.github.com>
2023-04-07 19:17:12 +0530

    Integration of translation with API

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outPut = document.querySelector("#output");
+// var serverUrl ="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverUrl ="https://api.funtranslations.com/translate/minion.json";
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("Somthing is wrong with the server try after some time");
+}
+
+function gettingUrl(text){
+    return serverUrl + "?text=" + text;
+}
 
 function clickHandler() {
-    outPut.innerText = "djhkjghslkgrlj " + txtInput.value;
+    fetch(gettingUrl(txtInput.value))
+        .then(response => response.json())
+        // .then(json => console.log(json.contents.translated))
+        .then(json => outPut.innerText = json.contents.translated)
+        .catch(errorHandler)
 }
 
 
